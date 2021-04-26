42ec484f554ae45bbe97d827a8d338034197ceaf
RuTokken <d.zaitsev91@mail.ru>
2021-04-26 10:20:38 +0300

    first commit

---
+++ task3/app/templatetags/news_filters.py
@@ -0,0 +1,56 @@
+import time
+from datetime import datetime
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def format_date(value):
+
+    time_diff = time.time() - value
+    if time_diff < 600:
+        return 'Меньше 10 минут назад'
+    elif 600 <= time_diff < 86400:
+        hours = int(time_diff/3600)
+        return f'Опубликовано {hours} часов назад'
+    elif time_diff >= 86400:
+        return datetime.fromtimestamp(value)
+
+
+@register.filter()
+def format_votes(value, default):
+
+    if value:
+        if value < -5:
+            return 'Всё плохо'
+        elif -5 <= value <= 5:
+            return 'Нейтрально'
+        elif value > 5:
+            return 'Хорошо'
+    else:
+        return default
+
+
+@register.filter
+def format_num_comments(value):
+
+    if value == 0:
+        return 'Оставьте комментарий'
+    elif 0 < value <= 50:
+        return value
+    elif value > 50:
+        return '50+'
+
+
+@register.filter()
+def format_text(value, count):
+
+    listed = value.split(' ')
+    front = ' '.join(listed[0:count])
+    back = ' '.join(listed[-count:])
+    return f'{front} ... {back}'
+
+
+
